6d69b3a254496379006f3abdd77aa537db0f5d9b
gamemill <TheGameMill@yahoo.com>
2022-01-12 11:51:49 +0000

    Updates

---
+++ precache-manifest.aea6d728888e128614d96e560a096fe1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "62787d3cdadb808613de",
+    "url": "/EPOS/static/js/main.62787d3c.chunk.js"
+  },
+  {
+    "revision": "c0728d90318f263118c8",
+    "url": "/EPOS/static/js/1.c0728d90.chunk.js"
+  },
+  {
+    "revision": "0b4733c25ca84db1710b",
+    "url": "/EPOS/static/js/2.0b4733c2.chunk.js"
+  },
+  {
+    "revision": "8a689d23ef22b858085d",
+    "url": "/EPOS/static/js/runtime~main.8a689d23.js"
+  },
+  {
+    "revision": "6954c8fa676228d5a0f7d2e1584501bc",
+    "url": "/EPOS/static/media/img1.6954c8fa.jpg"
+  },
+  {
+    "revision": "74d123d2dbc2be52ca319be2d6b28f20",
+    "url": "/EPOS/static/media/img2.74d123d2.jpg"
+  },
+  {
+    "revision": "5631a787a6af75a3fe49b06b8582729d",
+    "url": "/EPOS/static/media/img3.5631a787.jpg"
+  },
+  {
+    "revision": "ec73159db4145a9cd530228cdacc0ac2",
+    "url": "/EPOS/index.html"
+  }
+];
