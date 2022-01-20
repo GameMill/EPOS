85acea634026e53bd5e89d3aa950be0c0318c6d0
gamemill <TheGameMill@yahoo.com>
2022-01-20 12:23:40 +0000

    Update

--- src/Components/Admin/Pages/DashboardPage.js
+++ src/Components/Admin/Pages/DashboardPage.js
@@ -23,6 +23,7 @@ export default class Dashboard extends React.Component {
             {this.state.loading === true && <h2 className="mx-auto">Loading</h2> }
             {this.state.loading === false &&
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 row-cols-xxl-5 g-4">
+
                     <div class="col">
                         <div class="card text-dark bg-light mb-3 mx-auto" style={{ "maxWidth": "18rem" }}>
                             <h5 class="card-header">Profit Today <span className="float-end fs-"><i class="fas fa-truck text-white" style={{ padding: "5px", backgroundColor: "#8EB0C0", borderRadius: "10px" }}></i></span></h5>
@@ -33,6 +34,7 @@ export default class Dashboard extends React.Component {
                             </div>
                         </div>
                     </div>
+
                     <div class="col">
                         <div class="card text-dark bg-light mb-3 mx-auto" style={{ "maxWidth": "18rem" }}>
                             <h5 class="card-header">Sales Today <span className="float-end fs-"><i class="fas fa-truck text-white" style={{ padding: "5px", backgroundColor: "#8EB0C0", borderRadius: "10px" }}></i></span></h5>
@@ -76,6 +78,17 @@ export default class Dashboard extends React.Component {
                             </div>
                         </div>
                     </div>
+
+                    <div class="col">
+                        <div class="card text-dark bg-light mb-3 mx-auto" style={{ "maxWidth": "18rem" }}>
+                            <h5 class="card-header">Low Stock <span className="float-end fs-"><i class="fas fa-money-bill-wave text-white" style={{ padding: "5px", backgroundColor: "#8EB0C0", borderRadius: "5px" }}></i></span></h5>
+
+                            <div class="card-body ">
+                                <h5 class="card-title fs-2">-1</h5>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             }
         </div>
